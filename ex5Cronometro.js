be17a927077bc54ff065c7af7c0ddf754a086af3
Karine Lucietti <karine_lucietti@estudante.sc.senai.br>
2023-02-14 20:28:33 -0300

    first commit

---
+++ ex5Cronometro.js
@@ -0,0 +1,19 @@
+let startTime = 10
+let recurence
+counter.innerHTML = "Clique para iniciar"
+function countdown() {
+    if (startTime == 0) {
+        counter.innerHTML = "Seu tempo acabou!<br/>Tente novamente!"
+        console.log('Seu tempo acabou!! Tente novamente!!')
+        clearTimeout(recurence)
+    }
+    else {
+        let counter = document.getElementById("counter")
+        counter.innerText = startTime--
+        console.log(startTime)
+    }
+}
+function restart() {
+    startTime = 10
+    recurence = setInterval(countdown, 1000)
+}
